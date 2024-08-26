1069209bf4fb5734b052dfd4c1147eaa5b7758fa
Luiz Calaça <luizcalaca@gmail.com>
2024-08-26 01:01:25 -0300

    feat: organizando o model de Produtor

--- src/infrastructure/models/ProdutorModel.ts
+++ src/infrastructure/models/ProdutorModel.ts
@@ -1,13 +1,25 @@
 import Produtor from '../../domain/entities/Produtor';
 import PostgresAdapter from '../adapters/PostgresAdapter';
 
-export default class ProdutorModel {
-  constructor(private persistence: PostgresAdapter = new PostgresAdapter()) { }
-
+export default class ProdutorModel extends PostgresAdapter {
   public async create(entity: Produtor): Promise<void> {
-    await this.persistence.connection.query(
+    await this.connection.query(
       'INSERT INTO produtores (cpf_cnpj, nome_produtor) VALUES ($1, $2)',
       [entity.cpfCnpj, entity.nomeProdutor],
+    );
+  }
+
+  public async update(entity: Produtor): Promise<void> {
+    await this.connection.query(
+      'UPDATE produtores SET nome_produtor = $1 WHERE cpf_cnpj = $2',
+      [entity.nomeProdutor, entity.cpfCnpj],
+    );
+  }
+
+  public async delete(cpfCnpj: string): Promise<void> {
+    await this.connection.query(
+      'DELETE FROM produtores WHERE cpf_cnpj = $1',
+      [cpfCnpj],
     );
   }
 }
